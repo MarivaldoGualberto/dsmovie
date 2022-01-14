16a87f9b8a37285ca600dd8e986617496d4673a1
MarivaldoGualberto <marivaldo.rodrigues30@gmail.com>
2022-01-13 22:38:36 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -1,9 +1,13 @@
 package com.devsuperior.dsmovie.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -19,6 +23,9 @@ public class Movie {
 	private Integer count;
 	private String image;
 	
+	@OneToMany(mappedBy = "id.movie")
+	private Set<Score> scores =  new HashSet<>();
+			
 	public Movie() {
 	}
 
@@ -70,4 +77,7 @@ public class Movie {
 		this.image = image;
 	}
 
+	public Set<Score> getScores() {
+		return scores;
+	}	
 }
